001e7ed941731700546cb8c36c516083b8942ae2
Jin-youngdo <77922432+Jin-youngdo@users.noreply.github.com>
2021-11-12 05:46:23 +0900

    2021-11-12
    sockets[] 를 통해 각 브라우저의 socket을 전달받고
    전달받은 socket의 메세지들을 JSON을
    통해 nickname과 new_message로 구분

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,49 @@
+// WebSocket(브라우저와 서버 사이의 연결) 생성
 const socket = new WebSocket(`ws://${window.location.host}`);
+// html의 선택자들 받아오기
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
+// 브라우저로부터 json object를 수신, json object를 string 형변환 후
+// front-end -> back-end로 송신
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+// backend에서 보내온 event를 frontend에서 받기위한 작업
+socket.addEventListener("open", () => {
+  // socket open 이벤트
+  console.log("Connected to Server ✔");
+});
+
+// socekt message 이벤트(backend -> frontend 메시지 수신)
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data; // message의 data를 li에 넣어주기
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  // socket close 이벤트
+  console.log("Disconnected from Server ❌");
+});
+
+// 브라우저의 form에서 내용을 input 받을 시
+// input의 내용을 server(back-end)로 송신
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = ""; // 메세지를 보낼 때마다 비워주기
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -23,11 +23,20 @@ const server = http.createServer(app);
 // ws 패키지를 이용한 WebSocket 서버 구축(ws 서버)
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
+// 각 브라우저에서의 접속을 확인하기 위한 fake database 생성
+const sockets = [];
 
 // on 메소드 : backend에 연결된 사람의 정보 제공
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  sockets.push(socket); // sockets[]에 연결된 브라우저의 정보 저장
+  console.log("Connected to Browser ✔");
+  // 브라우저가 꺼질시 발생하는 이벤트(frontend -> backend 송신)
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  // 브라우저에서 메시지 송신 시 sockets 배열에 각 브라우저들의
+  // socket 정보를 저장해서 서버를 통해 브라우저와 통신
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
+});
 
 server.listen(3000, handleListen);
